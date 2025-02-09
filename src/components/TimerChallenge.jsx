e4ae808debf3057e1e875bafc4e047834f09cbae
Eddie <eocampo52@gmail.com>
2025-02-09 15:01:44 -0600

    Look over Evernote notes. Went over a lot. Went over new hook. useImperativeHandler. Went over Modal behavior in html. Went over timers in javascript

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useRef  } from 'react';
+import React, { useState, useRef } from 'react';
+import ResultModal from './ResultModal';
 
 // let timer;
 
-
 function TimerChallenge({ title, targetTime }) {
-  const timer = useRef()
+  const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -12,31 +13,34 @@ function TimerChallenge({ title, targetTime }) {
   function startHandler() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.open()
     }, targetTime * 1000);
     // The following line will excute right after the timer was set
     setTimerStarted(true);
   }
 
-function stopHandler() {
-  clearTimeout(timer.current)
-}
+  function stopHandler() {
+    clearTimeout(timer.current);
+  }
 
   return (
-    <section className='challenge'>
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className='challenge-time'>
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timerStarted ? stopHandler : startHandler}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : undefined}>
-        {timerStarted ? 'Time is running...' : 'Timer is inactive'}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+      <section className='challenge'>
+        <h2>{title}</h2>
+        <p className='challenge-time'>
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? stopHandler : startHandler}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer is inactive'}
+        </p>
+      </section>
+    </>
   );
 }
 
